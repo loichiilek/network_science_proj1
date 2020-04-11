f08b8f548cbb85211bddeb34620834d310fb1cbd
Jian Yang Seetoh <seet0064@berekley.edu>
2020-04-12 03:51:28 +0800

    added drawnetwork

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,8 @@
 # constructs the network
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+
 class Network:
   conferences = [
       'SIGMOD',
@@ -33,3 +37,19 @@ class Network:
 
   def setQuestions(self, new_quest):
     self.questions = new_quest
+
+  def drawNetwork(self):
+    conf = pd.read_csv ('authors.csv')
+    G=nx.DiGraph()
+
+    conf2 = conf.drop_duplicates()
+    conf2 = conf2.sort_values(by=['pid','year'])
+
+    # adding nodes:
+    G.add_nodes_from(self.conferences)
+
+    for i in range(len(conf2.index)-1):
+        if conf2['pid'].iloc[i] == conf2['pid'].iloc[i+1]:
+            G.add_edge(conf2['conf'].iloc[i+1], conf2['conf'].iloc[i])
+
+    nx.draw(G, with_labels = True)
